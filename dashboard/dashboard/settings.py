f7fa089bd933cabb6a0650e19828eb4ff40658d7
Luiz de Freitas <luizdefreitas10@gmail.com>
2023-03-08 11:50:49 -0300

    corrigindo configuracoes de servidor SMTP e envio de email de reset password

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -211,11 +211,14 @@ CORS_ALLOW_HEADERS = (
 # SMTP Configuration 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'luizdefreitas10@gmail.com'
-EMAIL_HOST_PASSWORD = 'luizinho16'
+EMAIL_HOST_PASSWORD = 'nvcrxsgbasgnkksf'
 
 
 AUTH_USER_MODEL = 'users.User'
+
+LOGIN_URL = '/login/'
--- dashboard/dashboard/urls.py
+++ dashboard/dashboard/urls.py
@@ -33,13 +33,17 @@ from users.views.register_view import CreateUserView
 urlpatterns = [
     path('api/v1/', include("users.urls")),
     path('api/v2/', include(router.urls)),
+    
     path('admin/', admin.site.urls),
+    
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    
     path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset_password/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    
     path('api/auth/register/', CreateUserView.as_view(), name='register'),
 ]

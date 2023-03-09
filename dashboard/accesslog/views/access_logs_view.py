13cf86c26bf6f3daf375076dfa845ed790457239
Luiz de Freitas <luizdefreitas10@gmail.com>
2023-03-09 15:27:03 -0300

    tentando implementacao do login e transferencia de dados de log para tabela no banco

---
+++ dashboard/accesslog/views/access_logs_view.py
@@ -0,0 +1,74 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from accesslog.models.user_logs import UserAccessLog
+from django.contrib import messages
+
+from django.contrib.auth.models import User
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            user_access_log = UserAccessLog(user=user)
+            user_access_log.save()
+            
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password')
+    else:
+        return render(request, 'registration/login.html')
+
+
+
+# import jwt
+# from django.shortcuts import render, redirect
+# from django.conf import settings
+# from django.contrib import messages
+# from django.contrib.auth import authenticate
+# from django.views.decorators.csrf import csrf_exempt
+
+
+# @csrf_exempt
+# def login(request):
+#     if request.method == 'POST':
+#         username = request.POST['username']
+#         password = request.POST['password']
+
+#         user = authenticate(username=username, password=password)
+
+#         if user is not None:
+#             payload = {
+#                 'id': user.id,
+#                 'username': user.username,
+#             }
+
+#             jwt_token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+
+#             request.session['jwt_token'] = jwt_token
+
+#             return redirect('admin')
+#         else:
+#             messages.error(request, 'Invalid username or password')
+
+#     return render(request, 'registration/login.html')
+
+
+# from django.views.generic import View
+# from django.http import HttpResponse
+
+
+# from accesslog.models.user_logs import UserAccessLog
+
+# # Create your views here.
+
+# class UserAccessLogView(View):
+    
+#     def post(self, request):
+#         if request.user.is_authenticated:
+#             UserAccessLog.objects.create(user=request.user, ip_address=request.META['REMOTE_ADDR'])
+#         return HttpResponse(status=204)
--- dashboard/users/models/user.py
+++ dashboard/users/models/user.py
@@ -41,18 +41,19 @@ class User(AbstractUser):
     
     # estudar sobre django signals para implementar corretamente a sobrescrita do metodo acima 
     
-class UserAccessLog(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='access_log')
-    access_date = models.DateTimeField(auto_now_add=True)
-    ip_address = models.GenericIPAddressField()
+# class UserAccessLog(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='access_log')
+#     access_date = models.DateTimeField(auto_now_add=True)
+#     ip_address = models.GenericIPAddressField()
     
     
-class UserRegistrationLog(models.Model):
-    added_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='added_users')
-    new_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='added_by_users')
-    registration_date = models.DateTimeField(auto_now_add=True)
+# class UserRegistrationLog(models.Model):
+#     added_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='added_users')
+#     new_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='added_by_users')
+#     registration_date = models.DateTimeField(auto_now_add=True)
     
 
-class PasswordResetLog(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='password_reset_log')
-    reset_date = models.DateTimeField(auto_now_add=True)
+# class PasswordResetLog(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='password_reset_log')
+#     reset_date = models.DateTimeField(auto_now_add=True)
+

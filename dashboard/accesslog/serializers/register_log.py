faecc303ece78cff5526cf55cfb3379eb6de0e21
Fernando Gouveia Soares <fernando_gouveia2@hotmail.com>
2023-03-10 11:12:25 -0300

    creating register logs

---
+++ dashboard/accesslog/serializers/register_log.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from models.user_logs import UserRegistrationLog
+
+class UserRegistrationLogSerializer(serializers.ModelSerializer):
+    model = UserRegistrationLog
+    fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name', 'registration_date']
+    
+    def create(self, validate_data):
+        log = UserRegistrationLog.objects.create_user(**validate_data)
+        return log
--- dashboard/users/views/register_view.py
+++ dashboard/users/views/register_view.py
@@ -1,6 +1,12 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+from django.shortcuts import render, redirect
+
+from dashboard.accesslog.models.user_logs import UserRegistrationLog
 from ..models import User
+from django.contrib.auth import authenticate
 
 from accesslog.utils.user_logs import log_user_access
 
@@ -12,5 +18,35 @@ class CreateUserView(generics.CreateAPIView):
     permission_classes = [AllowAny]
     serializer_class = UserRegisterSerializer
     
-    # def create_log(request):
-    #     log_user_access(request)
+    def create_register_log(request):
+        if request.user.is_authenticated:
+            if request.method == 'POST':
+                added_by = request.user
+            
+                username = request.POST['username']
+                password = request.POST['password']
+                email = request.POST['email']
+                first_name = request.POST['first_name']
+                last_name = request.POST['last_name']
+                telefone = request.POST['telefone']
+                userLog = is_valid(request, 
+                                    username=username,
+                                    password=password, 
+                                    email=email,
+                                    first_name=first_name,
+                                    last_name=last_name,
+                                    telefone=telefone,
+                                    added_by=added_by)
+
+                if userLog is not None:
+                    UserRegistrationLog.objects.create(user=userLog)
+                    
+                    return HttpResponseRedirect('login/')
+                else:
+                    messages.error(request, 'All obligatory fields must be filled.')
+            else:
+                return render(request, 'registration/register.html')
+            
+            
+                
+        log_user_access(request)
